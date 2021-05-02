44bb819a685cfb74de8d95f494d38b5dba1fe402
Haley Rairez <ramirezhaley@gmail.com>
2021-05-02 16:50:45 -0500

    finished easy 2

---
+++ Exercises/easy_2.rb
@@ -0,0 +1,169 @@
+#  def step(start, stop, step)
+#   current = start
+#   while current <= stop do
+#     yield(current)
+#     current += step
+#   end
+# end
+
+# step(1, 10, 3) { |value| puts "value = #{value}" }
+
+# def zip(arr1, arr2)
+#   zip_arr = []
+#   coutner = 0
+#   while coutner < arr1.size do
+#     zip_arr << [arr1[coutner], arr2[coutner]]
+#     coutner += 1
+#   end
+#   zip_arr
+# end
+
+# p zip([1, 2, 3], [4, 5, 6]) == [[1, 4], [2, 5], [3, 6]]
+
+# def map(arr)
+#   ret_arr = []
+#   arr.each{ |item| ret_arr << yield(item)}
+#   ret_arr
+# end
+
+# p map([1, 3, 6]) { |value| value**2 } == [1, 9, 36]
+# p map([]) { |value| true } == []
+# p map(['a', 'b', 'c', 'd']) { |value| false } == [false, false, false, false]
+# p map(['a', 'b', 'c', 'd']) { |value| value.upcase } == ['A', 'B', 'C', 'D']
+# p map([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]]
+
+# def count(*more)
+#   total = 0
+#   more.each{ |item| total += 1 if yield(item)}
+#   total
+# end
+
+# p count(1, 3, 6) { |value| value.odd? } == 2
+# p count(1, 3, 6) { |value| value.even? } == 1
+# p count(1, 3, 6) { |value| value > 6 } == 0
+# p count(1, 3, 6) { |value| true } == 3
+# p count() { |value| true } == 0
+# p count(1, 3, 6) { |value| value - 6 } == 3
+
+# def drop_while(arr)
+#   new_arr = arr.clone
+#   arr.each do |item| 
+#     if yield(new_arr.first)
+#       new_arr.shift
+#     else
+#       return new_arr
+#     end
+#   end
+#   new_arr
+# end
+
+# p drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
+# p drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
+# p drop_while([1, 3, 5, 6]) { |value| true } == []
+# p drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
+# p drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
+# p drop_while([]) { |value| true } == []
+
+# def each_with_index(arr)
+#   index = 0
+#   while index < arr.size
+#     yield(arr[index], index)
+#     index += 1
+#   end
+#   arr
+# end
+
+# result = each_with_index([1, 3, 6]) do |value, index|
+#   puts "#{index} -> #{value**index}"
+# end
+
+# puts result == [1, 3, 6]
+
+# def max_by(arr)
+#   return nil if arr.size == 0
+#   max_value = yield(arr.first) 
+#   max_item = arr.first
+#   arr.each do |item| 
+#     if yield(item) > max_value
+#       max_item = item
+#       max_value = yield(item)
+#     end
+#   end
+#   max_item
+# end
+
+# p max_by([1, 5, 3]) { |value| value + 2 } == 5
+# p max_by([1, 5, 3]) { |value| 9 - value } == 1
+# p max_by([1, 5, 3]) { |value| (96 - value).chr } == 1
+# p max_by([[1, 2], [3, 4, 5], [6]]) { |value| value.size } == [3, 4, 5]
+# p max_by([-7]) { |value| value * 3 } == -7
+# p max_by([]) { |value| value + 5 } == nil
+
+# def each_cons(arr)
+#   counter = 1
+#   while counter < arr.size
+#     yield(arr[counter - 1], arr[counter])
+#     counter += 1
+#   end
+
+#   nil
+# end
+
+# hash = {}
+# result = each_cons([1, 3, 6, 10]) do |value1, value2|
+#   hash[value1] = value2
+# end
+# p result == nil
+# p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+# hash = {}
+# result = each_cons([]) do |value1, value2|
+#   hash[value1] = value2
+# end
+# p hash == {}
+# p result == nil
+
+# hash = {}
+# result = each_cons(['a', 'b']) do |value1, value2|
+#   hash[value1] = value2
+# end
+# p hash == {'a' => 'b'}
+# p result == nil
+
+def each_cons(array, n)
+  array.each_index do |index|
+    break if index + n - 1 >= array.size
+    yield(*array[index..(index + n - 1)])
+  end
+  nil
+end
+
+hash = {}
+each_cons([1, 3, 6, 10], 1) do |value|
+  hash[value] = true
+end
+p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+
+hash = {}
+each_cons([1, 3, 6, 10], 2) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+each_cons([1, 3, 6, 10], 3) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6], 3 => [6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 4) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 5) do |value1, *values|
+  hash[value1] = values
+end
+p hash == {}
